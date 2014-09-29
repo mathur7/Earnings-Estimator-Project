41250e9ad53252067b21984d32f7b54a5fd99ebf
mathur7 <mathur7@gmail.com>
2014-09-29 09:58:35 -0700

    trying to debug calculate function - not able to pick up on the value of all services from calculateSum function

--- earnings.js
+++ earnings.js
@@ -1,16 +1,19 @@
 $( document ).ready(function() {
   
-  var calculateSum = $('select').change(function(){
+  function calculateSum(){
+  $('select').change(function(){
       var sum = 0; 
       $('select :selected').each(function() {
         sum += Number($(this).val());
       });   
       $("#sum").html(sum);
     });
-  
+    return sum;
+  }
+
   $("#calculate").click(function(){
     var deductions = Number($("#insurance").val()) + Number($("#maintainence").val());
-    var net_earnings = calculateSum - deductions;
+    var net_earnings = calculateSum() - deductions;
     $("#final_estimate").html(net_earnings);
   });
 
