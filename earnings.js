cc718834e4c73a0d5fd6a10d02509e51e1e5dc0d
mathur7 <mathur7@gmail.com>
2014-09-29 10:14:09 -0700

    trying something new to get the value of calculateSum function

--- earnings.js
+++ earnings.js
@@ -1,16 +1,17 @@
 $( document ).ready(function() {
   
-  function calculateSum(){
-  $('select').change(function(){
-      var sum = 0; 
+  function calculateSum(){  
+    $('select').change(function(){
+      var sum = 0;
       $('select :selected').each(function() {
         sum += Number($(this).val());
       });   
       $("#sum").html(sum);
-    });
-    return sum;
+    }); 
   }
 
+  calculateSum();
+  
   $("#calculate").click(function(){
     var deductions = Number($("#insurance").val()) + Number($("#maintainence").val());
     var net_earnings = calculateSum() - deductions;
