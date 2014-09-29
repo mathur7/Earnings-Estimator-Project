df59e00ce3e36c588267eef17729a7fba6092857
mathur7 <mathur7@gmail.com>
2014-09-29 12:21:29 -0700

    added some comments

--- earnings.js
+++ earnings.js
@@ -1,5 +1,9 @@
 $( document ).ready(function() {
   
+  //calculating the total earnings based on the services selected
+  //the change function is triggered when an option is selected
+  //the each function iterates through every option that is selected and adds the value of each option to the next option selected 
+  //and returns the sum of all options selected
   function calculateSum(){  
     $('select').change(function(){
       var sum = 0;
@@ -9,11 +13,12 @@ $( document ).ready(function() {
       $("#sum").html(sum);
     }); 
   }
-
   calculateSum();
   
+  //the calculate function takes the earnings and the deductions and returns the difference between the two as net earnings
   $("#calculate").click(function(){
     var deductions = Number($("#insurance").val()) + Number($("#maintainence").val());
+    //issue: calculateSum() function is not returning the value of the earnings at the moment
     var net_earnings = calculateSum() - deductions;
     $("#final_estimate").html(net_earnings);
   });
