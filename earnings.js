7ef3897b8f3e3b3c4664ccd15a1d180de0ca51b3
mathur7 <mathur7@gmail.com>
2015-11-30 14:25:10 -0800

    fixed js to calculate net earnings

--- earnings.js
+++ earnings.js
@@ -4,6 +4,17 @@ $( document ).ready(function() {
   //the change function is triggered when an option is selected
   //the each function iterates through every option that is selected and adds the value of each option to the next option selected 
   //and returns the sum of all options selected
+  
+    $("#calculate").click(function(){
+      console.log(calculateSum());
+      var maintainenceCost = Number($("#maintainence").val());
+      var insuranceCost = Number($("#insurance").val());
+      var deductions = maintainenceCost + insuranceCost;
+      var netEarnings = calculateSum() - deductions;
+      console.log(netEarnings);
+      $("#final_estimate").html(netEarnings);
+    });
+
   function calculateSum(){  
     $('select').change(function(){
       var sum = 0;
@@ -12,20 +23,11 @@ $( document ).ready(function() {
       });   
       $("#sum").html(sum);
     }); 
+    return sum.innerHTML;
   }
   calculateSum();
+});
   
   //the calculate function takes the earnings and the deductions and returns the difference between the two as net earnings
-  $("#calculate").click(function(){
-    var deductions = Number($("#insurance").val()) + Number($("#maintainence").val());
-    //issue: calculateSum() function is not returning the value of the earnings at the moment
-    var net_earnings = calculateSum() - deductions;
-    $("#final_estimate").html(net_earnings);
-  });
-
-});
-
-
-
-
-
+  
+    
